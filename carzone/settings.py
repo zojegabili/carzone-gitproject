a3bd8235cea7b62f2af6c65647736c12486d39b8
ZOJE <zoje.gabili@gmail.com>
2021-12-16 09:36:36 +0100

    update

--- carzone/settings.py
+++ carzone/settings.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS=[
 # Media settings
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Team
 
 # Create your views here.
 def home(request):
+    teams=Team.objects.all()
+    data={
+        'teams': teams,
+    }
     return render(request,'pages/home.html')
 
 def about(request):
